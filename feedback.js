0a4c7920cf1797d13d2bc4b1d10e221941f98522
Niklas von Hertzen <niklasvh@gmail.com>
2012-09-23 00:13:55 +0300

    added license info

--- feedback.js
+++ feedback.js
@@ -1,3 +1,10 @@
+/*
+ feedback.js <http://experiments.hertzen.com/jsfeedback/>
+ Copyright (c) 2012 Niklas von Hertzen. All rights reserved.
+ http://www.twitter.com/niklasvh
+
+ Released under MIT License
+*/
 (function( window, document, undefined ) {
 if ( window.Feedback !== undefined ) { 
     return; 
---
+++ src/license.txt
@@ -0,0 +1,7 @@
+/*
+ feedback.js <http://experiments.hertzen.com/jsfeedback/>
+ Copyright (c) 2012 Niklas von Hertzen. All rights reserved.
+ http://www.twitter.com/niklasvh
+
+ Released under MIT License
+*/
